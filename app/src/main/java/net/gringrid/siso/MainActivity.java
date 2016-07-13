fd0e54b813b8006e92cbae9bf4ee4ca7ecf97e93
choijiho <choijiho.korea@gmail.com>
2016-07-13 13:03:36 +0900

    Add color

--- app/src/main/java/net/gringrid/siso/MainActivity.java
+++ app/src/main/java/net/gringrid/siso/MainActivity.java
@@ -1,13 +1,19 @@
 package net.gringrid.siso;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPink));
+
     }
 }
