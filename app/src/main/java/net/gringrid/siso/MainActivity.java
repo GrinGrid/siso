8121f56772928100a5958b063f75fbee1d0af75c
jiho.choi <choijiho.korea@gmail.com>
2016-07-13 11:22:01 +0900

    Create Project SISO

---
+++ app/src/main/java/net/gringrid/siso/MainActivity.java
@@ -0,0 +1,13 @@
+package net.gringrid.siso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
